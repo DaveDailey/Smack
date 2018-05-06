1afefac575c1ebfc6cb1b24b41467abad68a6c79
Dave Dailey <dave@davedailey.com>
2018-05-06 11:35:42 -0500

    swreveal added

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by DAVID DAILEY on 5/6/18.
+//  Copyright © 2018 davedailey. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+
+   
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by DAVID DAILEY on 5/6/18.
+//  Copyright © 2018 davedailey. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+    self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+  
+    
+
+    
+
+}
