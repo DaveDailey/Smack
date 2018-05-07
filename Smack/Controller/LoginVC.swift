2bd304076c0d8583fb28bf4b6f47e5a3fdbc6f65
Dave Dailey <dave@davedailey.com>
2018-05-07 07:51:38 -0500

    finished lesson 4

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by DAVID DAILEY on 5/7/18.
+//  Copyright © 2018 davedailey. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    
+    @IBAction func exitLogin(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by DAVID DAILEY on 5/7/18.
+//  Copyright © 2018 davedailey. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let SHOW_LOGIN = "showLogin"
