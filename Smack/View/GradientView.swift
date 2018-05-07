1c86f0312a54ad6b554ad75046696280f4af4117
Dave Dailey <dave@davedailey.com>
2018-05-07 07:13:58 -0500

    lesson 3 complete

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by DAVID DAILEY on 5/7/18.
+//  Copyright © 2018 davedailey. All rights reserved.
+//
+
+import UIKit
+//IBDesignable means its able to work in storyboard
+@IBDesignable
+class GradientView: UIView {
+    //IBinspectable lets us access/change variable inside of storyboard
+    @IBInspectable var topColor: UIColor  = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.7346998826, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
