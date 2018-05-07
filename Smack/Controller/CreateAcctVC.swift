82c580d4ef2ea02f71070b06f97515d6b6d558ac
Dave Dailey <dave@davedailey.com>
2018-05-07 16:58:59 -0500

    lesson 10 is where i live

--- Smack/Controller/CreateAcctVC.swift
+++ Smack/Controller/CreateAcctVC.swift
@@ -9,7 +9,28 @@
 import UIKit
 
 class CreateAcctVC: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var userEmailTxt: UITextField!
+    @IBOutlet weak var userPasswordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = userEmailTxt.text , userEmailTxt.text != "" else {return}
+        guard let pass = userPasswordTxt.text , userPasswordTxt.text != "" else {return}
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND_TO_CHANNEL, sender: nil)
     }
